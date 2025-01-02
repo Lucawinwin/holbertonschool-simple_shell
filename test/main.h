7fc77b1290c5c394896b627f48f30dead7adad7f
Lucawinwin <windwehrluca@gmail.com>
2025-01-02 14:02:23 +0100

    add files

---
+++ test/main.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
